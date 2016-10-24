b9d6cfd825bc0de3d5cd09e4bde648891bcc8725
Patrick Steinhauer <patrick.steinhauer@haw-hamburg.de>
2016-10-24 11:04:50 +0200

    Finish Stored Procedure.

---
+++ 0 - doc/1 - Aufgabe/CREATE_PRODUKTIONSAUFTRAG.sql
@@ -0,0 +1,32 @@
+--------------------------------------------------------
+--  Datei erstellt -Montag-Oktober-24-2016   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Procedure CREATE_PRODUKTIONSAUFTRAEGE
+--------------------------------------------------------
+set define off;
+
+  CREATE OR REPLACE EDITIONABLE PROCEDURE "ABQ295"."CREATE_PRODUKTIONSAUFTRAEGE" AS
+
+BEGIN
+  --if SYSTIMESTAMP = '23:59:59' then
+
+    INSERT INTO PRODUKTIONSAUFTRAG(BESTELLNUMMER, MONAT)
+      SELECT BST.BESTELLNUMMER, to_char(BST.DATUM, 'yyyy')
+      FROM BESTELLUNG BST,
+           BESTELLPOSITION BSP
+      WHERE BST.BESTELLNUMMER = BSP.BESTELLNUMMER
+      AND   BST.DATUM < TRUNC(SYSDATE) + INTERVAL '23:59:59' HOUR TO SECOND
+      AND   BST.BESTELLNUMMER not in (SELECT P.BESTELLNUMMER FROM PRODUKTIONSAUFTRAG P);
+    
+/*      SELECT * 
+  FROM BESTELLUNG BST, 
+       BESTELLPOSITION BSP
+  WHERE BST.BESTELLNUMMER = BSP.BESTELLNUMMER
+  AND   BST.DATUM < TRUNC(SYSDATE) + INTERVAL '23:59:59' HOUR TO SECOND;
+    */
+    
+  --end if;
+END CREATE_PRODUKTIONSAUFTRAEGE;
+
+/
